7d09dc0ddcea4e486fe45b956dbfd9d4052d6664
TechnoBro03 <76632885+TechnoBro03@users.noreply.github.com>
2023-02-13 04:47:43 -0600

    Update AdvancedButton.h

--- src/AdvancedButton.h
+++ src/AdvancedButton.h
@@ -1,3 +1,10 @@
+///
+/// This code is written and maintained by TechnoBro03.
+///
+/// This header file serves as the detailed documentation
+///     for the AdvancedButton library.
+///
+
 #pragma once
 
 #include <Arduino.h>
@@ -6,105 +13,104 @@ class AdvancedButton {
 
     private:
 
-        /**
-         * User Defined Variables
-         */
-        int pin; // Input pin
-        bool defaultState; // When not pressed, input pin is HIGH or LOW.
-        int countMode; // When button is "toggled".
-        unsigned long debounceTime; // Time delay to remove unwanted noise.
-        unsigned long toggleDelay; // Time allowed between multiple toggles.
-
-        /**
-         * User accessable Variables (get)
-         */
-        unsigned int count; // Total number of toggles.
-        unsigned int numPresses; // Number of consecutive toggles.
-        bool currentState; // Current state of button, without debounce.
-        bool currentDebounceState; // Current state of button, with debounce.
-        unsigned long lengthPressed; // Length button has been, or was previously, pressed.
-
-        /**
-         * Internal
-         */
-        unsigned long lastTimeCountChanged;
+        ///
+        /// User Defined Variables
+        ///
+        int pin;
+        bool defaultState;
+        int countMode;
+        unsigned long debounceDelay;
+        unsigned long pressDelay;
+        ///
+        /// User Accessable Variables
+        ///
+        unsigned int count;
+        unsigned int numPresses;
+        bool currentState;
+        bool currentDebounceState;
+        unsigned long lengthPressed;
+        ///
+        /// Internal
+        ///
         bool previousState;
         unsigned long lastTimeStateChanged;
         bool previousDebounceState;
         unsigned long currentTime;
         unsigned long timePressed;
         unsigned long timeReleased;
-        void press();
 
     public:
 
-        /**
-         * Constructor.
-         *
-         * @param pin Arduino pin connected to button.
-         * @param defaultState Logic level of button when not pressed (HIGH or LOW).
-         * @param pullup Usage of internal pullup resistor.
-         * @param countMode When the button is "toggled." Button state, NOT logic level; pressed / unpressed. (FALLING, RISING, CHANGE).
-         * @param debounceTime Time delay to remove unwanted noise.
-         * @param toggleDelay Time allowed between consecutive toggles.
-         */
-        AdvancedButton(int pin, bool defaultState = HIGH, bool pullup = true, int countMode = RISING, unsigned long debounceTime = 10ul, unsigned long toggleDelay = 50ul);
+        ///
+        /// Constructor.
+        ///
+        /// \param pin Arduino pin connected to button.
+        /// \param defaultState Logic level of button when not pressed (HIGH or LOW).
+        /// \param pullup Usage of internal pull-up resistor.
+        /// \param countMode When the button is "toggled." Button state (pressed or unpressed), NOT logic level. (FALLING, RISING, CHANGE).
+        /// \param debounceDelay Time delay to remove ripple signal.
+        /// \param pressDelay Time allowed between consecutive presses.
+        ///
+        AdvancedButton(int pin, int countMode = RISING, bool defaultState = HIGH, bool internalPullup = true, unsigned long debounceDelay = 10ul, unsigned long pressDelay = 250ul);
 
-        /**
-         * Returns current state of button.
-         *
-         * @return true if pressed, false otherwise.
-         */
+        ///
+        /// Returns current state of button.
+        ///
+        /// \return true if pressed, false otherwise.
+        ///
         bool getState();
 
-        /**
-         * Returns current state of button, with debounce.
-         *
-         * @return true if pressed, false otherwise.
-         */
+        ///
+        /// Returns current state of button, with debounce.
+        ///
+        /// \return true if pressed, false otherwise.
+        ///
         bool getDebounceState();
 
-        /**
-         * Returns total number of times button has been toggled.
-         *
-         * @param resetCount If true, after calling this function, count is reset.
-         *
-         * @return number of times toggled.
-         */
+        ///
+        /// Returns total number of times button has been toggled.
+        ///
+        /// \param resetCount If true, count is set to 0.
+        ///
+        /// \return number of times toggled.
+        ///
         unsigned int getCount(bool resetCount = false);
 
-        /**
-         * Returns number of consecutive toggles.
-         *
-         * A consecutive toggle is any time a toggle (countMode) occurs within toggleDelay ms of the previous.
-         *
-         * @return number of consecutive toggles. If 0, not enough time has passed (toggleDelay)
-         *   to determine if toggling action has finished.
-         */
-        unsigned int getMultiPress();
+        ///
+        /// Returns number of consecutive presses.
+        ///
+        /// A consecutive press is any time a press occurs within 'pressDelay'ms of the previous.
+        ///
+        /// \param resetNumPresses If true, and a value other than 0 is returned, numPresses is set to 0.
+        ///
+        /// \return Number of presses. 0 is returned if the button is still being pressed repeatadly
+        ///     ('pressDelay'ms need to pass without a press)
+        ///
+        unsigned int getNumPresses(bool resetNumPresses = false);
 
-         /**
-         * Returns length button has been, or was previously, pressed.
-         *
-         * @param resetLengthPressed If true, after calling this function, lengthPressed is reset.
-         *
-         * @return Length pressed. If button is still being pressed, the elapsed time pressed is returned.
-         */
-        unsigned long getLengthPressed(bool resetLengthPressed = true);
+        ///
+        /// Returns length button has been, or was previously, pressed.
+        ///
+        /// \param resetLengthPressed If true, lengthPressed is set to 0.
+        ///
+        /// \return Length pressed. If button is still being pressed, the elapsed time pressed is returned.
+        ///
+        unsigned long getLengthPressed(bool resetLengthPressed = false);
 
-        /**
-         * Determines if length pressed falls within given time interval.
-         *
-         * @param min Minimun time button must be pressed.
-         * @param max Maximum time button can be pressed.
-         * @param hold If false, button must be released to return true. If true, button must be pressed to return true.
-         *
-         * @return true if length falls within time interval, false otherwise.
-         */
-        bool getLongPress(unsigned long min, unsigned long max, bool hold = false);
+        ///
+        /// Determines if the button was pressed for a given length of time.
+        ///
+        /// \param min Minimum time button must be pressed.
+        /// \param max Maximum time button can be pressed.
+        /// \param pressed If false, button must NOT be pressed to return true. If true, button must be pressed to return true.
+        /// \param resetLongPress If true, and this function returns true, lengthPressed is set to 0 (next call will return false unless button is pressed again).
+        ///
+        /// \return true if length falls within the given time interval, false otherwise.
+        ///
+        bool getLongPress(unsigned long min, unsigned long max, bool pressed = false, bool resetLongPress = true);
         
-        /**
-         * Must be called to use any method other than getState().
-         */
+        ///
+        /// Must be called first to use any method other than getState().
+        ///
         void update();
 };
